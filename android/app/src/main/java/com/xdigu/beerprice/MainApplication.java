99efdae297a0a65803d16ce2b15a5768a94e7be3
Rodrigo Jorge <xdigu@live.com>
2020-02-29 19:44:14 -0300

    resolving problems with android package

--- android/app/src/main/java/com/xdigu/beerprice/MainApplication.java
+++ android/app/src/main/java/com/xdigu/beerprice/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.facebook.react.BuildConfig;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
